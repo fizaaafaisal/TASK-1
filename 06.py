438c0073c3d92637bc938499564b9531e91a7c6b
fizaaafaisal <fathimafizafaisal@gmail.com>
2024-08-20 16:09:41 +0530

    06.py

---
+++ 06.py
@@ -0,0 +1,10 @@
+#youngest person
+fahma=10
+farin=8
+farha=5
+if("fahma<farin") and ("fahma>farha"):
+    print("fahma is greater ")
+elif farin>farha:
+    print("farin is greater")
+else:
+    print("farha is greater")
