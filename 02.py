d02e1da8e9585454492f25f8ba7dd9186d9f6e7d
fizaaafaisal <fathimafizafaisal@gmail.com>
2024-08-20 15:53:49 +0530

    02.py

---
+++ 02.py
@@ -0,0 +1,7 @@
+#Take a value of length and breadth of ractangle from the user and check whether it is square 
+L=int(input("enter the length of the rectangle:"))
+B=int(input("enter the breadth of the rectangle:"))
+if L*B:
+    print("it is a rectangle")
+else:
+    print("it is not a rectangle")
