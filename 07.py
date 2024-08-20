c8f18c2f1d7e16ab2a112da94a8b7e635b95f03c
fizaaafaisal <fathimafizafaisal@gmail.com>
2024-08-20 16:12:57 +0530

    07.py

---
+++ 07.py
@@ -0,0 +1,8 @@
+#check wether number is { +,-,0 }
+num=int(input("ENTER THE NUMBER : "))
+if num<0:
+    print("number is negative")
+elif num>0:
+    print("number is postive")
+else:
+    print("number is zero")
